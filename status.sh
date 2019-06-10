b1d047e50475f234510ccfcffc6cd2391a1bbf9d
Rex <ezekiel7176@gmail.com>
2019-06-10 20:13:10 +0200

    Update status.sh

--- status.sh
+++ status.sh
@@ -8,7 +8,15 @@ zenity --title="Debug $TIME" \
 --height=800 \
 --width=800 \
 --text-info \
+--ok-label="OK" \
+--cancel-label="Reload" \
 --filename=$path/status.log
 )
+
+case $? in
+        0) exit 0 ;;
+        1) $path/status.sh ;;
+       -1) echo "An unexpected error has occurred." ;;
+esac
 
 exit 0
